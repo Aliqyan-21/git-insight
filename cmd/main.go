c549c6ef46b9ae4979d9fb6894c1da1db5e44d2b
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-12-02 02:48:49 +0530

    feat: better formatting and events handling

--- cmd/main.go
+++ cmd/main.go
@@ -45,6 +45,27 @@ func fetchUserEvents(username string) ([]Event, error) {
 	return events, nil
 }
 
+func checkEvents(typeOfEvent string) bool {
+	// definition of relevant events for us
+	relevantEvents := map[string]bool{
+		"PushEvent":         true,
+		"PullRequestEvent":  true,
+		"IssuesEvent":       true,
+		"CreateEvent":       true,
+		"DeleteEvent":       true,
+		"ForkEvent":         true,
+		"WatchEvent":        true,
+		"ReleaseEvent":      true,
+		"MemberEvent":       true,
+		"PublicEvent":       true,
+		"GollumEvent":       true,
+		"IssueCommentEvent": true,
+		"LabelEvent":        true,
+		"MilestoneEvent":    true,
+	}
+	return relevantEvents[typeOfEvent]
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: git-insight <username>")
@@ -60,6 +81,39 @@ func main() {
 
 	fmt.Printf("Events fetched successfully: \n\n")
 	for _, event := range events {
-		fmt.Printf("%s: %s\n", event.Type, event.Repo.Name)
+		if !checkEvents(event.Type) {
+			continue
+		}
+
+		switch event.Type {
+		case "PushEvent":
+			fmt.Printf("📤 Pushed to %s\n", event.Repo.Name)
+		case "PullRequestEvent":
+			fmt.Printf("🔄 Opened a Pull Request in %s\n", event.Repo.Name)
+		case "IssuesEvent":
+			fmt.Printf("📝 Created or commented on an issue in %s\n", event.Repo.Name)
+		case "CreateEvent":
+			fmt.Printf("✨ Created a new repository: %s\n", event.Repo.Name)
+		case "DeleteEvent":
+			fmt.Printf("🗑️Deleted something in: %s\n", event.Repo.Name)
+		case "ForkEvent":
+			fmt.Printf("🍴 Forked the repository: %s\n", event.Repo.Name)
+		case "WatchEvent":
+			fmt.Printf("⭐ Started watching the repository: %s\n", event.Repo.Name)
+		case "ReleaseEvent":
+			fmt.Printf("🚀 Released a new version in: %s\n", event.Repo.Name)
+		case "MemberEvent":
+			fmt.Printf("👥 A member was added or removed in: %s\n", event.Repo.Name)
+		case "PublicEvent":
+			fmt.Printf("🌍 Made the repository public: %s\n", event.Repo.Name)
+		case "GollumEvent":
+			fmt.Printf("📖 Updated the wiki in: %s\n", event.Repo.Name)
+		case "IssueCommentEvent":
+			fmt.Printf("💬 Commented on an issue or pull request in: %s\n", event.Repo.Name)
+		case "LabelEvent":
+			fmt.Printf("🏷️ A label was added or modified in: %s\n", event.Repo.Name)
+		case "MilestoneEvent":
+			fmt.Printf("🎯 A milestone was created or updated in: %s\n", event.Repo.Name)
+		}
 	}
 }
